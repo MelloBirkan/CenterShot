701ffe4b674f4ca26ee7f8e93434509dbb37ffa7
Mello Birkan <mgbirkan@icloud.com>
2023-10-13 00:28:44 -0300

    Initial Commit

---
+++ CenterShot/CenterShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  CenterShotApp.swift
+//  CenterShot
+//
+//  Created by Marcello Gonzatto Birkan on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CenterShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
