758c0b45ed80de17e7b555fbd26dd8ef80a49a27
Mello Birkan <mgbirkan@icloud.com>
2023-10-13 23:47:53 -0300

    Primeiras adições na tela principal.

--- CenterShot/ContentView.swift
+++ CenterShot/ContentView.swift
@@ -8,17 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO 89")
+      Text("89")
+      HStack {
+        Text("1")
+        Slider(value: .constant(50), in: 1.0...100.0)
+        Text("100")
+      }
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
