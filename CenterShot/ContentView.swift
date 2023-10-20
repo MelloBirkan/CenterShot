944231cff92871e6b636d69d04bee4bdb842a49d
Mello Birkan <mgbirkan@icloud.com>
2023-10-20 19:49:13 -0300

    `States` adicionados.

--- CenterShot/ContentView.swift
+++ CenterShot/ContentView.swift
@@ -8,15 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var alertIsVisible: Bool = false
+  @State private var sliderValue: Double = 50.0
+  private var guessNumber = 89
+  
   var body: some View {
     VStack {
-      Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO 89")
-      Text("89")
+      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        .bold()
+        .multilineTextAlignment(.center)
+        .lineSpacing(4.0)
+        .font(.footnote)
+        .kerning(2)
+      Text("\(guessNumber)")
+        .font(.largeTitle)
+        .fontWeight(.black)
+        .kerning(-1)
       HStack {
         Text("1")
-        Slider(value: .constant(50), in: 1.0...100.0)
+          .bold()
+        Slider(value: self.$sliderValue, in: 1.0...100)
         Text("100")
+          .bold()
       }
+      Button("Hit Me") {
+        alertIsVisible = true
+      }
+      .alert(
+        "Hello there",
+        isPresented: $alertIsVisible,
+        actions: {
+          Button("Awsome") {
+            print("Alert closed")
+          }
+        },
+        message: {
+          Text("The value is: \(sliderValue)")
+        })
     }
   }
 }
