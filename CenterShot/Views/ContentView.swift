e530d70b427daa5c8b3f23dab5ca8012c6f7cb9c
Mello Birkan <mgbirkan@icloud.com>
2023-10-30 08:50:40 -0300

    View principal subdividia em `Componentes`

--- CenterShot/Views/ContentView.swift
+++ CenterShot/Views/ContentView.swift
@@ -13,42 +13,66 @@ struct ContentView: View {
   @State private var game = Game()
   
   var body: some View {
-    VStack {
-      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-        .bold()
-        .multilineTextAlignment(.center)
-        .lineSpacing(4.0)
-        .font(.footnote)
-        .kerning(2)
-      Text(String(game.target))
-        .font(.largeTitle)
-        .fontWeight(.black)
-        .kerning(-1)
-      HStack {
-        Text("1")
+    ZStack {
+      Color("BackgroundColor")
+        .ignoresSafeArea()
+      VStack {
+        Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
           .bold()
-        Slider(value: self.$sliderValue, in: 1.0...100)
-        Text("100")
-          .bold()
+          .multilineTextAlignment(.center)
+          .lineSpacing(4.0)
+          .font(.footnote)
+          .kerning(2)
+          .padding(.horizontal, 30)
+          .foregroundColor(Color("TextColor"))
+        Text(String(game.target))
+          .font(.largeTitle)
+          .fontWeight(.black)
+          .kerning(-1)
+          .foregroundColor(Color("TextColor"))
+        HStack {
+          Text("1")
+            .bold()
+            .foregroundColor(Color("TextColor"))
+          Slider(value: self.$sliderValue, in: 1.0...100)
+          Text("100")
+            .bold()
+            .foregroundColor(Color("TextColor"))
+        }
+        .padding()
+        Button(action: {
+            alertIsVisible = true
+        }) {
+            Text("Hit Me".uppercased())
+                .bold()
+                .font(.title3)
+                .padding(20)
+                .foregroundColor(.white)
+                .background(
+                    ZStack {
+                        Color("ButtonColor")
+                        LinearGradient(colors: [Color.white.opacity(0.3), Color.clear], startPoint: .top, endPoint: .bottom)
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 21))
+                )
+        }
+
+        .alert(
+          "Hello there",
+          isPresented: $alertIsVisible,
+          actions: {
+            Button("Awsome") {
+              print("Alert closed")
+            }
+          },
+          message: {
+            let roundedValue = Int(sliderValue.rounded())
+            Text("""
+              The slider's value is: \(roundedValue)
+              You Scored \(game.points(sliderValue: roundedValue))
+            """)
+          })
       }
-      Button("Hit Me") {
-        alertIsVisible = true
-      }
-      .alert(
-        "Hello there",
-        isPresented: $alertIsVisible,
-        actions: {
-          Button("Awsome") {
-            print("Alert closed")
-          }
-        },
-        message: {
-          let roundedValue = Int(sliderValue.rounded())
-          Text("""
-            The slider's value is: \(roundedValue)
-            You Scored \(game.points(sliderValue: roundedValue))
-          """)
-        })
     }
   }
 }
---
+++ CenterShot/Views/TextViews.swift
@@ -0,0 +1,18 @@
+//
+//  TextViews.swift
+//  CenterShot
+//
+//  Created by Marcello Gonzatto Birkan on 25/10/23.
+//
+
+import SwiftUI
+
+struct TextViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextViews()
+}
