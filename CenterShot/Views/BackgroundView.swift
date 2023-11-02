265b2f914bfdfcb447236db8db28b20dc65bdca1
Mello Birkan <mgbirkan@icloud.com>
2023-11-02 01:46:18 -0300

    Limpando a `ContentView `

---
+++ CenterShot/Views/BackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundView.swift
+//  CenterShot
+//
+//  Created by Marcello Gonzatto Birkan on 30/10/23.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundView()
+}
---
+++ CenterShot/Views/RoundedViews.swift
@@ -0,0 +1,18 @@
+//
+//  RoundedViews.swift
+//  CenterShot
+//
+//  Created by Marcello Gonzatto Birkan on 30/10/23.
+//
+
+import SwiftUI
+
+struct RoundedViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoundedViews()
+}
